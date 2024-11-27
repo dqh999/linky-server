53e1845ffdea9218b3949e83f3f12dfdb962528b
dqh999 <dqhit999@gmail.com>
2024-11-27 15:15:48 +0700

    first commit

---
+++ src/main/java/com/example/linky_server/LinkyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.linky_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/linky_server/LinkyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linky_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
